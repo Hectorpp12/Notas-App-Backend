2f90be57236ccbd46b8545bfddbf92d6d365b593
Hector Sanchez <133878232+Hectorpp12@users.noreply.github.com>
2025-11-30 14:21:13 -0400

    Backend de notas base terminada

---
+++ src/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import notesRoutes from './routes/notesRoutes.js';
+import { connectDB } from './config/db.js';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173'
+}))
+
+app.use(express.json());
+
+app.use("/api/notes", notesRoutes)
+
+const PORT = process.env.PORT || 3001;
+
+connectDB()
+
+.then(() => {
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port http://localhost:${PORT}`);
+    })
+
+})
